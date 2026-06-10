7e0cdf993c201b25f043a91f689709b1d9334147
Pcdep <phuclocle2@gmail.com>
2026-06-10 16:35:51 +0700

    cập nhật lại code

--- Presentation/FrmSanPham.cs
+++ Presentation/FrmSanPham.cs
@@ -23,7 +23,6 @@ namespace Presentation
                 dgvSanPham.CellClick += DgvSanPham_CellClick;
             }
 
-            LoadDuLieuGiaLap();
         }
 
         private void LoadDuLieuGiaLap()
